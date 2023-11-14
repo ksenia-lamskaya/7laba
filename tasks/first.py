60f76eb8e34abf167bc157f157deede48acd50de
Ksenia <klamskaa@gmail.com>
2023-11-15 01:00:59 +0300

    add all

---
+++ tasks/first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    print('Введите элементы списка: ')
+    lst = [input() for _ in range(10)]
+    element = input('Введите элемент: ')
+    index = []
+
+    for ind, value in enumerate(lst):
+        if value == element:
+            index.append(ind)
+            
+    if len(index) == 0:
+        print("Такого элемента в списке нет")
+    else:
+        print('Индексы этого элемента: ')
+        [print(i) for i in index]
---
+++ tasks/second.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    numbers = [int(input('Введите элементы списка: ')) for _ in range(10)]
+
+    # 1. минимальный по модулю элемент списка
+    min_abs = min(numbers, key=abs)
+    print("Минимальный по модулю элемент списка:", abs(min_abs))
+
+    # 2. сумма модулей элементов списка после первого элемента, равного нулю
+    if 0 in numbers:
+        zero_index = numbers.index(0)
+        sum_abs = sum(abs(x) for x in numbers[zero_index+1:])
+        print("Сумма модулей элементов списка, расположенных после первого элемента, равного нулю:", sum_abs)
+    else:
+        print('В списке нет элемента 0')
